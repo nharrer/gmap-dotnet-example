3709cc57a154346416514996eb07aaeb4128cff8
Norbert Harrer <nharrer@equilaris.at>
2018-05-24 14:08:34 +0200

    Initial Version

---
+++ MapTest/TestForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace MapTest
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // mapBrowser
+            // 
+            this.mapBrowser.AllowNavigation = false;
+            this.mapBrowser.AllowWebBrowserDrop = false;
+            this.mapBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.mapBrowser.Location = new System.Drawing.Point(12, 12);
+            this.mapBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.mapBrowser.Name = "mapBrowser";
+            this.mapBrowser.ScriptErrorsSuppressed = true;
+            this.mapBrowser.ScrollBarsEnabled = false;
+            this.mapBrowser.Size = new System.Drawing.Size(736, 485);
+            this.mapBrowser.TabIndex = 2;
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 509);
+            this.Controls.Add(this.mapBrowser);
+            this.Name = "TestForm";
+            this.Text = "TestForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser mapBrowser;
+    }
+}
+
---
+++ MapTest/TestForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace MapTest
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            FixBrowserEmulation();
+
+            InitializeComponent();
+
+            bool designTime = LicenseManager.UsageMode == LicenseUsageMode.Designtime;
+            if (!designTime) {
+                mapBrowser.ScriptErrorsSuppressed = false;
+
+                string docFile = Path.Combine(Application.StartupPath, "maptest.html");
+                string documentText = File.ReadAllText(docFile);
+                mapBrowser.DocumentText = documentText;
+            }
+        }
+
+        // see: 
+        // https://stackoverflow.com/questions/17922308/use-latest-version-of-internet-explorer-in-the-webbrowser-control
+        // https://blog.malwarebytes.com/101/2016/01/a-brief-guide-to-feature_browser_emulation/
+        private static void FixBrowserEmulation()
+        {
+            var appName = Process.GetCurrentProcess().ProcessName + ".exe";
+
+            // 11000 (0x2AF8) - Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed 
+            // in IE11 edge mode. Default value for IE11.
+            int? mode = 0x2AF8;
+
+            try {
+                const string regpath = @"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION";
+                using (RegistryKey regkey = Registry.CurrentUser.CreateSubKey(regpath)) {
+                    if (regkey == null) {
+                        Debug.WriteLine("Error: Can not access: " + @"HKEY_CURRENT_USER\\" + regpath);
+                        return;
+                    }
+
+                    var currentMode = regkey.GetValue(appName) as int?;
+                    if (currentMode == mode) {
+                        Debug.WriteLine("Info: FEATURE_BROWSER_EMULATION is correct.");
+                        return;
+                    }
+
+                    regkey.SetValue(appName, mode, RegistryValueKind.DWord);
+
+                    currentMode = regkey.GetValue(appName) as int?;
+                    if (currentMode == mode) {
+                        Debug.WriteLine("Info: FEATURE_BROWSER_EMULATION set to " + currentMode);
+                    } else {
+                        Debug.WriteLine("Info: FEATURE_BROWSER_EMULATION modification failed. Current value: " + currentMode);
+                    }
+                }
+            } catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
